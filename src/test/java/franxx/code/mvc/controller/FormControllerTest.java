0c9ab5eafd18672e09606a81d3655f055738ab25
Hilmi AM <imiia75775@proton.me>
2024-06-28 22:16:08 +0700

    request content type

---
+++ src/test/java/franxx/code/mvc/controller/FormControllerTest.java
@@ -0,0 +1,35 @@
+package franxx.code.mvc.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FormControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void formSubmit() throws Exception {
+        mockMvc.perform(
+                post("/form/hello")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("name", "mee")
+        ).andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("Hai mee"))
+        );
+    }
+}
