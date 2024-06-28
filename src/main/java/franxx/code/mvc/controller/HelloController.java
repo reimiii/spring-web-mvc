83a3bde77d09288b18e29f3e878a20c11dbee21e
Hilmi AM <imiia75775@proton.me>
2024-06-28 15:05:30 +0700

    servlet request and response

--- src/main/java/franxx/code/mvc/controller/HelloController.java
+++ src/main/java/franxx/code/mvc/controller/HelloController.java
@@ -1,16 +1,23 @@
 package franxx.code.mvc.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Controller
 public class HelloController {
 
     @RequestMapping(path = "hello")
-    public void helloWorld(HttpServletResponse response) throws IOException {
-        response.getWriter().println("hello world");
+    public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String parameter = request.getParameter("name");
+        if (Objects.isNull(parameter)) {
+            parameter = "guest";
+        }
+
+        response.getWriter().println("hello " + parameter);
     }
 }
