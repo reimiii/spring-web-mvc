e09b803a174e1afd4dff5b9fb7b3c94100e8fd74
Hilmi AM <imiia75775@proton.me>
2024-06-28 21:24:40 +0700

    mock bean

--- src/main/java/franxx/code/mvc/controller/HelloController.java
+++ src/main/java/franxx/code/mvc/controller/HelloController.java
@@ -1,23 +1,24 @@
 package franxx.code.mvc.controller;
 
+import franxx.code.mvc.service.HelloService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
-import java.util.Objects;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping(path = "hello")
+    @Autowired
+    private HelloService helloService;
+
+    @RequestMapping(path = "/hello")
     public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String parameter = request.getParameter("name");
-        if (Objects.isNull(parameter)) {
-            parameter = "guest";
-        }
-
-        response.getWriter().println("hello " + parameter);
+        String hai = helloService.hai(parameter);
+        response.getWriter().println(hai);
     }
 }
---
+++ src/test/java/franxx/code/mvc/controller/HelloControllerMockTest.java
@@ -0,0 +1,42 @@
+package franxx.code.mvc.controller;
+
+import franxx.code.mvc.service.HelloService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerMockTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private HelloService helloService;
+
+    @BeforeEach
+    void setUp() {
+        Mockito.when(helloService.hai(Mockito.anyString()))
+                .thenReturn("hai guys");
+    }
+
+    @Test
+    void helloMee() throws Exception {
+        mockMvc.perform(get("/hello").queryParam("name", "Mee"))
+                .andExpectAll(
+                        status().isOk(),
+                        content().string(Matchers.containsString("hai guys"))
+                );
+    }
+}
