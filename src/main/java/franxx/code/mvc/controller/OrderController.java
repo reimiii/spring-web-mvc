bb9e450f78d686ad790880154221abadb22732d7
Hilmi AM <imiia75775@proton.me>
2024-06-29 20:04:10 +0700

    path variable

---
+++ src/main/java/franxx/code/mvc/controller/OrderController.java
@@ -0,0 +1,17 @@
+package franxx.code.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class OrderController {
+
+    @GetMapping(path = "orders/{orderId}/products/{productId}")
+    @ResponseBody
+    public String order(@PathVariable("orderId") String orderId, @PathVariable("productId") String productId) {
+        return "order: " + orderId + ", product: " + productId;
+    }
+
+}
