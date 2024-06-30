b57b449700eaa32f52a77f08308d51402e794941
Hilmi AM <imiia75775@proton.me>
2024-06-30 14:35:43 +0700

    json

---
+++ src/main/java/franxx/code/mvc/controller/PersonApiController.java
@@ -0,0 +1,22 @@
+package franxx.code.mvc.controller;
+
+import franxx.code.mvc.model.CreatePersonRequest;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PersonApiController {
+
+    @PostMapping(
+            path = "/api/person",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseBody
+    public CreatePersonRequest createPersonRequest(@RequestBody CreatePersonRequest request) {
+        return request;
+    }
+}
---
+++ src/test/java/franxx/code/mvc/controller/PersonApiControllerTest.java
@@ -0,0 +1,58 @@
+package franxx.code.mvc.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import franxx.code.mvc.model.CreatePersonRequest;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PersonApiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @SneakyThrows
+    void createPerson() {
+        CreatePersonRequest request = CreatePersonRequest.of(
+                "mee",
+                "mail@me.com",
+                "0101",
+                CreatePersonRequest.Address.of("bogor", "id"),
+                List.of("kodng", "mkn"),
+                List.of(
+                        CreatePersonRequest.SocialMedia.of("fb", "fb.com"),
+                        CreatePersonRequest.SocialMedia.of("ig", "ig.com")
+                )
+        );
+
+        String jsonRequest = objectMapper.writeValueAsString(request);
+
+        mockMvc.perform(
+                post("/api/person")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest)
+        ).andExpectAll(
+                status().isOk(),
+                content().json(jsonRequest)
+        );
+    }
+}
