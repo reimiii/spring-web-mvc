9f1a2264f1f61b5a1c1dd0902a706f9cdb05ccee
Hilmi AM <imiia75775@proton.me>
2024-06-30 17:05:13 +0700

    mvc config

---
+++ src/main/java/franxx/code/mvc/MyWebConf.java
@@ -0,0 +1,8 @@
+package franxx.code.mvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebConf implements WebMvcConfigurer {
+}
