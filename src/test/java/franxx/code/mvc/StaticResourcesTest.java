1ea08fff1bf0615751ab7fd14c0c23d627f5022a
Hilmi AM <imiia75775@proton.me>
2024-07-01 14:19:36 +0700

    static resources

---
+++ src/test/java/franxx/code/mvc/StaticResourcesTest.java
@@ -0,0 +1,31 @@
+package franxx.code.mvc;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StaticResourcesTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void getStatic() throws Exception {
+    mockMvc.perform(
+          get("/index.html")
+    ).andExpectAll(
+          status().isOk(),
+          content().string(Matchers.containsString("hello pacman static"))
+    );
+  }
+}
