399cfe3cdeec6cfff5e6c1a5644671914d78af69
Hilmi AM <imiia75775@proton.me>
2024-07-01 14:41:37 +0700

    model and view

--- src/main/java/franxx/code/mvc/controller/HelloController.java
+++ src/main/java/franxx/code/mvc/controller/HelloController.java
@@ -9,19 +9,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Controller
 public class HelloController {
 
-    @Autowired
-    private HelloService helloService;
+  @Autowired
+  private HelloService helloService;
 
-    @GetMapping(path = "/hello")
-    public void helloWorld(@RequestParam(name = "name", required = false) String name,
-                           HttpServletResponse response) throws IOException {
-        String hai = helloService.hai(name);
-        response.getWriter().println(hai);
-    }
+  @GetMapping(path = "/hello")
+  public void helloWorld(@RequestParam(name = "name", required = false) String name,
+                         HttpServletResponse response) throws IOException {
+    String hai = helloService.hai(name);
+    response.getWriter().println(hai);
+  }
+
+  @GetMapping(path = "/web/hello")
+  public ModelAndView hello(@RequestParam(name = "name", required = false) String name) {
+    return new ModelAndView("hello", Map.of(
+          "title", "learn view",
+          "name", name
+    ));
+  }
 }
--- src/test/java/franxx/code/mvc/controller/HelloControllerTest.java
+++ src/test/java/franxx/code/mvc/controller/HelloControllerTest.java
@@ -7,40 +7,50 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.MockMvcBuilder.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class HelloControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-    @Test
-    void helloGuest() throws Exception {
-        mockMvc.perform(get("/hello"))
-                .andExpectAll(
-                        status().isOk(),
-                        content().string(Matchers.containsString("hello guest"))
-                );
-    }
+  @Test
+  void helloGuest() throws Exception {
+    mockMvc.perform(get("/hello"))
+          .andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("hello guest"))
+          );
+  }
 
-    @Test
-    void helloMee() throws Exception {
-        mockMvc.perform(get("/hello").queryParam("name", "Mee"))
-                .andExpectAll(
-                        status().isOk(),
-                        content().string(Matchers.containsString("hello Mee"))
-                );
-    }
-    @Test
-    void methodPost() throws Exception {
-        mockMvc.perform(post("/hello").queryParam("name", "Mee"))
-                .andExpectAll(
-                        status().isMethodNotAllowed()
-                );
-    }
+  @Test
+  void helloMee() throws Exception {
+    mockMvc.perform(get("/hello").queryParam("name", "Mee"))
+          .andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("hello Mee"))
+          );
+  }
+
+  @Test
+  void methodPost() throws Exception {
+    mockMvc.perform(post("/hello").queryParam("name", "Mee"))
+          .andExpectAll(
+                status().isMethodNotAllowed()
+          );
+  }
+
+  @Test
+  void getMustache() throws Exception {
+    mockMvc.perform(get("/web/hello").queryParam("name", "Mee"))
+          .andExpectAll(
+                status().isOk(),
+                content().string(Matchers.containsString("hello Mee")),
+                content().string(Matchers.containsString("learn view"))
+          );
+  }
+
 }
