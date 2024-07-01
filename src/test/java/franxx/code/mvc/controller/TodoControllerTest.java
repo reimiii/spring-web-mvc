8680ec575736ac850914eb18ca519d2516dae4e8
Hilmi AM <imiia75775@proton.me>
2024-07-01 15:25:55 +0700

    rest controller

---
+++ src/test/java/franxx/code/mvc/controller/TodoControllerTest.java
@@ -0,0 +1,48 @@
+package franxx.code.mvc.controller;
+
+import lombok.SneakyThrows;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TodoControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  @SneakyThrows
+  void addTodos() {
+    mockMvc.perform(
+          post("/todos")
+                .accept(MediaType.APPLICATION_JSON)
+                .param("todo", "mee")
+    ).andExpectAll(
+          status().isOk(),
+          content().string(Matchers.containsString("mee"))
+    );
+  }
+  @Test
+  @SneakyThrows
+  void getTodos() {
+    mockMvc.perform(
+          get("/todos")
+                .accept(MediaType.APPLICATION_JSON)
+    ).andExpectAll(
+          status().isOk(),
+          content().string(Matchers.containsString("mee"))
+    );
+  }
+}
