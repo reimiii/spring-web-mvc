27f3054476d18cdc907ed9bec50c08a1bc595013
Hilmi AM <imiia75775@proton.me>
2024-07-01 15:59:57 +0700

    rest template

--- src/main/java/franxx/code/mvc/SpringWebMvcApplication.java
+++ src/main/java/franxx/code/mvc/SpringWebMvcApplication.java
@@ -2,12 +2,25 @@ package franxx.code.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class SpringWebMvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringWebMvcApplication.class, args);
-	}
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder
+          .setConnectTimeout(Duration.ofSeconds(2L))
+          .setReadTimeout(Duration.ofSeconds(2L))
+          .build();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringWebMvcApplication.class, args);
+  }
 
 }
